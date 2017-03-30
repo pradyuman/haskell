8f0dd7d50a18559982c670d1c79ec0d1fa0ea092
Pradyuman Vig <pradyumanvig@outlook.com>
2017-03-30 03:59:13 -0500

    add treeDelete

--- tree.hs
+++ tree.hs
@@ -10,6 +10,23 @@ treeInsert x (Node a left right)
   | x < a  = Node a (treeInsert x left) right
   | x > a  = Node a left (treeInsert x right)
 
+treeDelete :: (Ord a) => a -> Tree a -> Tree a
+treeDelete _ EmptyNode = EmptyNode
+treeDelete x (Node a left right)
+  | x == a = delete (Node a left right)
+  | x < a  = Node a (treeDelete x left) right
+  | x > a  = Node a left (treeDelete x right)
+
+delete :: (Ord a) => Tree a -> Tree a
+delete (Node _ EmptyNode right) = right
+delete (Node _ left EmptyNode)  = left
+delete (Node _ left right) = Node maxValue (treeDelete maxValue left) right
+  where maxValue = treeMax left
+
+treeMax :: (Ord a) => Tree a -> a
+treeMax (Node a _ EmptyNode) = a
+treeMax (Node a _ right) = treeMax right
+
 treeElem :: (Ord a) => a -> Tree a -> Bool
 treeElem _ EmptyNode = False
 treeElem x (Node a left right)
