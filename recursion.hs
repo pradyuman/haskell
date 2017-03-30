1d5cdf26e0efdf5b08d811b4ab16ee8e45bf98b8
Pradyuman Vig <pradyumanvig@outlook.com>
2017-03-30 00:04:10 -0500

    add zipWith, map, and filter

--- recursion.hs
+++ recursion.hs
@@ -11,7 +11,7 @@ replicate' n x
 take' :: (Num a, Ord a) => a -> [b] -> [b]
 take' _ [] = []
 take' n (x:xs)
-  | n <= 0 = []
+  | n <= 0    = []
   | otherwise = x: take' (n-1) xs
 
 reverse' :: [a] -> [a]
@@ -27,15 +27,30 @@ zip' [] _ = []
 zip' _ [] = []
 zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
 
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
 elem' :: (Eq a) => a -> [a] -> Bool
 elem' _ [] = False
 elem' e (x:xs)
-  | e == x = True
-  | otherwise =  elem' e xs
+  | e == x    = True
+  | otherwise = elem' e xs
 
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) =
   quicksort smaller ++ [x] ++ quicksort bigger
   where smaller = [a | a <- xs, a <= x]
-        bigger = [b | b <- xs, b > x]
+        bigger  = [b | b <- xs, b > x]
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+  | f x       = x : filter' p xs
+  | otherwise = filter' p xs
